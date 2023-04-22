c57d4bed40a17a4d356842c96986fc6516e12521
mayur <mayurnandwalkar0@gmail.com>
2023-04-22 20:39:03 +0530

    buttons added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import './App.css';
 import Home from './Pages/Home';
+import './App.css';
 
 function App() {
   return (
     <>
-      <Home/>
+      <Home />
     </>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from '../components/Header'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
     <>
-    <Header/>
+      <div class="main_body">
+        <video autoplay muted loop>
+          <source src="assets/img/Final Moonetized Background.mp4" />
+        </video>
+        <div class="overlay_wrapper">
+          <Header />
+          <HeroSection />
+        </div>
+      </div>
     </>
   )
 }
