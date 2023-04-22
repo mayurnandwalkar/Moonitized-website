f8c4af569ff68bd0396890b584051daf79fbcf68
mayur <mayurnandwalkar0@gmail.com>
2023-04-23 00:37:32 +0530

    seller-dashaboard page design fixed

--- src/Pages/SellerDashabord.js
+++ src/Pages/SellerDashabord.js
@@ -3,6 +3,10 @@ import Header from '../components/Header'
 import ProfileLogo from '../images/profileLogo.png'
 import SheetImg from '../images/sheetImg.png'
 import { BsThreeDots } from 'react-icons/bs';
+import { AiOutlineEye } from "react-icons/ai";
+import { MdModeEditOutline } from 'react-icons/md';
+import { AiFillPlusCircle } from 'react-icons/ai';
+
 
 const SellerDashabord = () => {
     return (
@@ -13,32 +17,34 @@ const SellerDashabord = () => {
                     <div className='container'>
                         <div class="container-fluid">
                             <div class="row">
-                                <div class="d-none d-md-block col-md-5 col-lg-4 col-xl-3">
-                                    <div class="card mb-4 p-4">
+                                <div class="d-md-block col-md-5 col-lg-4 col-xl-4">
+                                    <div class="card mb-4 p-4 cst-border-white">
                                         <div class="pt-3">
                                             <ul class="navbar-nav">
                                                 <div className='text-center mb-4'>
-                                                    <img src={ProfileLogo} />
-                                                    <h4 className='mt-3'>Stephen johnson</h4>
+                                                    {/* <img src={ProfileLogo} className='rounded-circle'/> */}
+                                                    <img class="rounded-circle shadow-4-strong" alt="avatar2" src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp" />
+                                                    <h4 className='mt-3 text-white'>Stephen johnson <MdModeEditOutline /></h4>
+                                                    <p className='text-white'>@stephenjhon <MdModeEditOutline /></p>
                                                 </div>
-                                                <button>Edit Profile Settings</button>
-                                                <hr />
-                                                <div className='row'>
-                                                    <div className='col-sm'>From</div>
-                                                    <div className='col-sm'>United States</div>
+                                                <button type="button" class="btn border-white text-white"><AiOutlineEye />&nbsp; &nbsp; &nbsp;&nbsp; Edit Profile Settings</button>
+                                                <hr style={{ "color": "white" }} />
+                                                <div className='row '>
+                                                    <div className='col-sm text-white'>From</div>
+                                                    <div className='col-sm text-white'>United States</div>
                                                 </div>
                                                 <div className='row'>
-                                                    <div className='col-sm'>Member Since</div>
-                                                    <div className='col-sm'>Apr 2023</div>
+                                                    <div className='col-sm text-white'>Member Since</div>
+                                                    <div className='col-sm text-white'>Apr 2023</div>
                                                 </div>
                                             </ul>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-7 col-lg-8 col-xl-9 ">
-                                    <div class="card mb-4  p-3">
+                                <div class="col-md-7 col-lg-8 col-xl-8 ">
+                                    <div class="card mb-4 cst-border-white p-3">
                                         <div class="row">
-                                            <div class="col-md-2 col-3">
+                                            <div class="col-md-2 col-6">
                                                 <h4 class="dashboard-heading4 pt-2">Product Listed</h4>
                                             </div>
                                             <div class="col-md-7 col-6">
@@ -46,11 +52,10 @@ const SellerDashabord = () => {
                                             </div>
                                         </div>
                                     </div>
-
                                     <div class="mb-4">
                                         <div class="row">
                                             <div class="col-xl-4 col-md-6 mb-4 ">
-                                                <div class="card" style={{ "width": "18rem" }}>
+                                                <div class="card" >
                                                     <img src={SheetImg} className='card-img-top' />
                                                     <div class="card-body" style={{ "background": "#124D40" }}>
                                                         <h5 class="card-title text-center text-white">I will do financial modeling spreadsheet for you</h5>
@@ -62,8 +67,8 @@ const SellerDashabord = () => {
                                                 </div>
                                             </div>
                                             <div class="col-xl-4 col-md-6 mb-4 ">
-                                                <div class="card" style={{ "width": "18rem" }}>
-                                                    <img src={SheetImg} className='card-img-top' />
+                                                <div class="card" >
+                                                    <img src={SheetImg} className='`card-img-top`' />
                                                     <div class="card-body" style={{ "background": "#124D40" }}>
                                                         <h5 class="card-title text-center text-white">I will do financial modeling spreadsheet for you</h5>
                                                         <div className='row'>
@@ -74,21 +79,15 @@ const SellerDashabord = () => {
                                                 </div>
                                             </div>
                                             <div class="col-xl-4 col-md-6 mb-4 ">
-                                                <div class="card" style={{ "width": "18rem" }}>
-                                                    <img src={SheetImg} className='card-img-top' />
-                                                    <div class="card-body" style={{ "background": "#124D40" }}>
-                                                        <h5 class="card-title text-center text-white">I will do financial modeling spreadsheet for you</h5>
-                                                        <div className='row'>
-                                                            <div className='col text-white'><BsThreeDots /></div>
-                                                            <div className='col text-white'>Starting at $50</div>
-                                                        </div>
+                                                <div class="card pt-5 pb-4" style={{ "background": "#124D40", "align-items": "center" }}>
+                                                    <AiFillPlusCircle style={{ "font-size": "100px", "color": "white" }} />
+                                                    <div class="card-body" >
+                                                        <h5 class="card-title text-center text-white">CREATE  A NEW PRODUCT</h5>
                                                     </div>
                                                 </div>
                                             </div>
-                                            
                                         </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
