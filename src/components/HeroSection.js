f2f518af1534c748b7e8247c12c242576d3a5d97
mayur <mayurnandwalkar0@gmail.com>
2023-04-26 22:21:21 +0530

    on edit profie model tootgle for passowrd added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const HeroSection = () => {
+    const navigate = useNavigate();
     return (
         <>
             <section className="hero_wrapper">
@@ -9,7 +11,7 @@ const HeroSection = () => {
                         <p className="hero_title"> Take Crypto Payments Easily </p>
                         <h4 className="hero_detail">Connect your site or app and have your customers pay via crypto coins and tokens today!</h4>
                         <div className="hero_btnz">
-                            <a href='#' className="flex_align hero_btn bg_faq" >I want to sell</a>
+                            <a href='#' className="flex_align hero_btn bg_faq" onClick={()=>navigate("/seller-dashaboard")}>I want to sell</a>
                             <a href='#' className="flex_align hero_btn bg_signup"  >I want to buy</a>
                         </div>
                     </div>
--- src/models/EditProfileSetting.js
+++ src/models/EditProfileSetting.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Select from 'react-select'
 import { IoMdClose } from "react-icons/io";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import { MdEdit } from "react-icons/md";
+import { useState } from 'react';
 
 const options = [
     { value: 'BTC', label: 'BTC' },
@@ -9,7 +12,52 @@ const options = [
     { value: 'BNB', label: 'BNB' },
 ]
 
+const customStyles = {
+    control: (provided, state) => ({
+        ...provided,
+        backgroundColor: '#2C133B',
+        color: 'white',
+        fontFamily: 'Roboto',
+        fontSize: 16
+    }),
+    singleValue: (provided, state) => ({
+        ...provided,
+        color: 'white',
+        fontFamily: 'Roboto',
+        fontSize: 16
+    }), menuList: (provided, state) => ({
+        ...provided,
+        backgroundColor: '#5C245C',
+        fontFamily: 'Roboto',
+        fontSize: 16
+    }),
+    placeholder: (provided, state) => ({
+        ...provided,
+        color: 'white',
+        fontSize: 16
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        backgroundColor: state.isFocused ? '#B186D2' : '#3A1740',
+        color: 'white',
+        fontFamily: 'Roboto',
+        fontSize: 15
+    }),
+    singleValue: (provided, state) => {
+        const opacity = state.isDisabled ? 0.5 : 1;
+        const transition = 'opacity 300ms';
+
+        return { ...provided, opacity, transition };
+    }
+};
+
 const EditProfileSetting = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(prevState => !prevState);
+    };
+
     return (
         <>
 
@@ -21,24 +69,58 @@ const EditProfileSetting = () => {
                         </div>
                         <div class="modal-body mx-3">
                             <h3 className='text-white text-center mb-5'>Edit profile settings</h3>
-                            <div className='row p-2 px-3 ' >
-                                <input class="col-12 form-control clas-cat text-right" type="email" placeholder='Email Address' />
+
+                            <div className='row p-2'>
+                                <div class="input-container">
+                                    <input class="col-12 form-control clas-cat text-right" type='email' placeholder='Email Address' style={{ textAlign: 'right', paddingRight: "10px" }} />
+                                </div>
                             </div>
-                            <div className='row p-2 px-3 ' >
-                                <input class="col-12 form-control clas-cat text-right" type="email" placeholder='Password Update' />
+
+                            <div className='row p-2'>
+                                <div className="input-container">
+                                    <input
+                                        className="col-12 form-control clas-cat text-right"
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder='Password Update'
+                                        style={{ textAlign: 'right', paddingRight: "34px" }}
+                                    />
+                                    <span className="icon-container" onClick={togglePasswordVisibility}>
+                                        {showPassword ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                                    </span>
+                                </div>
                             </div>
-                            <div className='row p-2 px-3 ' >
-                                <input class="col-12 form-control clas-cat text-right" type="email" placeholder='Secret Key' />
+                            <div className='row p-2'>
+                                <div className="input-container">
+                                    <input
+                                        className="col-12 form-control clas-cat text-right"
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder='Secret Key'
+                                        style={{ textAlign: 'right', paddingRight: "34px" }}
+                                    />
+                                    <span className="icon-container" onClick={togglePasswordVisibility}>
+                                        {showPassword ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                                    </span>
+                                </div>
                             </div>
+
                             <div className='row p-2 px-3 ' >
                                 <button class="btn btn-new-key-genrate">Generate New secret Key</button>
                             </div>
-                            <div className='row p-2 px-3 ' >
-                                <input class="col-12 form-control clas-cat text-right" type="email" placeholder='Wallet' />
+                            <div className='row p-2'>
+                                <div class="input-container">
+                                    <input class="col-12 form-control clas-cat text-right" type="text" placeholder='Wallet' value="Metamask" style={{ textAlign: 'right', paddingRight: "34px" }} />
+                                    <span class="icon-container">
+                                        <MdEdit />
+                                    </span>
+                                </div>
                             </div>
                             <div className='row pt-2 px-1' >
                                 <div className='col-12'>
-                                    <Select class="" isMulti options={options} placeholder="Select Crypto Lines" />
+                                    <Select class="multislect-cst"
+                                        styles={customStyles}
+                                        isMulti options={options}
+                                        placeholder="Select Crypto Lines"
+                                    />
                                 </div>
                             </div>
                             <div className='row mt-4 mb-5 p-3' >
