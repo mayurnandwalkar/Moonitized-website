ff8e0aee87989126fb271c1df81d4e10c6208712
mayur <mayurnandwalkar0@gmail.com>
2023-05-01 19:25:18 +0530

    01-05-2023

--- src/Pages/SingleProductPage.js
+++ src/Pages/SingleProductPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ListingHomePageHeader from '../components/ListingHomePageHeader'
 import CategoryMenus from '../components/CategoryMenus'
 import SliderCst from '../components/SliderCst';
+import { BsHeart } from 'react-icons/bs';
 
 const SingleProductPage = () => {
     function eventLogger(ev) {
@@ -17,7 +18,7 @@ const SingleProductPage = () => {
                     <div className='container'>
                         <div class="row">
                             <div class=" col-6 product-images-wrapper">
-                                <div class="row">
+                                <div class="row mt-5">
                                     <div className='col'>
                                         <div class="thumbs-wrapper hide-for-mobile">
                                             <div class="thumb-image active">
@@ -34,7 +35,7 @@ const SingleProductPage = () => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='col-10'>
+                                    <div className='col-9 '>
                                         <SliderCst />
                                     </div>
                                 </div>
@@ -42,14 +43,25 @@ const SingleProductPage = () => {
 
                             </div>
 
-                            <div class="col-6 product-details-wrapper text-white">
-                                <p class="product">Sneaker Company</p>
-                                <h1 class="text-white">Fall Limited Edition Sneakers</h1>
-                                <p class="product-description">
+                            <div class="col-6 product-details-wrapper text-white mt-5">
+                                <div className='row mb-2'>
+                                    <div className='col-sm-3'>
+
+                                    <select className='col-3 form-select sasasa select-arrow search-for-anything '>
+                                        <option>USD$100</option>
+                                    </select>
+                                    </div>
+
+                                </div>
+                                <h2 class="text-white">iWatch 2 - NEW</h2>
+                                <p className='cst-font-tahoma'>Tech and more store <BsHeart/></p>
+                                <p className='cst-font-tahoma'>Description:</p>
+                                <p class="product-description cst-font-tahoma">
                                     These low-profile sneakers are your perfect casual wear companion.
                                     Featuring a durable rubber outer sole, they’ll withstand
                                     everything the weather can offer.
                                 </p>
+                                <button className='btn btn-success col-sm-6 mt-5'>Buy Now</button>
                             </div>
                         </div>
 
--- src/components/SliderCst.js
+++ src/components/SliderCst.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, ImageWithZoom, ButtonFirst, ButtonLast, DotGroup } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import { AiOutlineLeftCircle, AiOutlineRightCircle } from 'react-icons/ai';
+
+
+const buttonStyles = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    backgroundColor: 'transparent',
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+    fontSize: '24px',
+    color: '#fff',
+};
+
+const leftButtonStyles = {
+    ...buttonStyles,
+    left: '0',
+};
+
+const rightButtonStyles = {
+    ...buttonStyles,
+    right: '0',
+};
 
 const SliderCst = () => {
     return (
@@ -32,8 +56,8 @@ const SliderCst = () => {
                         <img src="https://mohamedaridah.github.io/frontendmentor_e-commerce-product-page/images/image-product-2.jpg" />
                     </Slide>
                 </Slider>
-                <ButtonBack>Back</ButtonBack>
-                <ButtonNext>Next</ButtonNext>
+                <ButtonBack style={leftButtonStyles}><AiOutlineLeftCircle /></ButtonBack>
+                <ButtonNext style={rightButtonStyles}><AiOutlineRightCircle /></ButtonNext>
             </CarouselProvider>
         </>
     )
